0fc4d10a2ab1f0348776be1299bd58c34a2b6617
Maurizio D'Addona <mauritiusdadd@gmail.com>
2026-06-19 09:10:03 +0200

    Fix typos

--- main_functions/main_sextractor.py
+++ main_functions/main_sextractor.py
@@ -322,7 +322,7 @@ def setup(args):
                         help='Parameters table suffix')
     parser.add_argument('--multiband_catalogue_suffix', type=str, action='store', default='multiband.forced.cat',
                         help='Multiband catalogue suffix')
-    parser.add_argument('--verbose_type', type=str, default='QUITE',
+    parser.add_argument('--verbose_type', type=str, default='QUIET',
                         help='Set sextractor output verbosity')
     args = parser.parse_args(args)
 
